5a1edf98910a567f41e62ec7781011997421c7bf
Shrabin00007 <shrabin8@gmail.com>
2026-06-25 17:26:23 +0545

    Update day3.py

--- day3.py
+++ day3.py
@@ -24,31 +24,50 @@
 
 
 #-------------------------------------------Dictionary & Set in python----------------------------------------------------------
-info = {
-    "key" : "value",
-    "name" : "bibek",
-    "learning" : "coding",
-    "age" : 20,
-    "is adult" : True,
-    "subject" : ["python","Mysql","numpie"],
-    "topics" : ("dict", "set")
-}
+# info = {
+#     "key" : "value",
+#     "name" : "bibek",
+#     "learning" : "coding",
+#     "age" : 20,
+#     "is adult" : True,
+#     "subject" : ["python","Mysql","numpie"],
+#     "topics" : ("dict", "set")
+# }
 
-print(info["topics"])
+# print(info["topics"])
 
-#nested dictonary
-student = {
-    "name" : "bibek",
+# #nested dictonary
+# student = {
+#     "name" : "bibek",
     
-    "subject" : {
-        "math" : 67,
-        "science" : 77,
-        "english" : 102,
-        "optmath" : 54
-    },
-    "rollno" : 2
+#     "subject" : {
+#         "math" : 67,
+#         "science" : 77,
+#         "english" : 102,
+#         "optmath" : 54
+#     },
+#     "rollno" : 2
+
+# }
+# print(student["subject"]["math"])
+
+# print("bibek")
+
+
+
+bibek = {
+    "name" : "Bibek_Parajuli",
+    "age" : 20,
+    "p_address": "lamjung",
+    "t_address" : "ktm"
 
 }
-print(student["subject"]["math"])
 
-print("bibek")
+bibek["name"] = "Shrabin00007"
+bibek["age"] = 21
+bibek["subject"] = {
+    "math" : 22,
+    "sci" : 21
+}
+
+print(bibek["subject"])
