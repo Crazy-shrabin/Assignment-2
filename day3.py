77feafac708c8bc39741d95028a357ef389e1948
Shrabin00007 <shrabin8@gmail.com>
2026-06-26 00:57:44 +0545

    Update day3.py

--- day3.py
+++ day3.py
@@ -95,9 +95,72 @@
 # Set is the collection of the unordered items.
 # Each elements in the set must be unique & immutable.
 
-mums = {1,2,3,4}
-set2 = {1,2,2,2}
+# mums = {1,2,3,4}
+# set2 = {1,2,2,2}
 
 #repeted elements stored only once, so it restores to {1,2}
 
-null_set = set()      #empty set syntax
+# null_set = set()      #empty set syntax
+
+# set1 = {1,2,3,4,2,2,2, "Hello","bibek"}
+# print(set1)
+# print(type(set1))
+
+connection = () #it creates the empty set.
+
+
+
+
+#-----------------------Set method----------------------------
+# set.add(el) #adds an element
+# set.remove(el) #removes the element
+# set.clear() #empties the set
+# set.pop()  #remove the random value
+
+# sets ------->mutable
+# sets(elements) --------------->immutable
+
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(2)
+# collection.add("bibek")
+# collection.add((1,2,3,4,5))
+# print(collection)
+
+
+
+#----------------------------------Set method-----------------------------
+# set.union(set2)    #combines both set values & return new
+# set.intersection(set2)     #combines common values & return new
+
+
+
+
+
+# set2 = {1,2,3,4,5}
+# set3 = {4,3,5,9,8,7,6}
+
+# print(set2.union(set3))
+# print(set2.intersection(set3))
+# dict = {
+#     "table" : ["a piece of furniture","list of facts and figure"],
+#     "cat" : "a small animal"
+# }
+# print(dict)
+
+classes = {"python", "java", "C++", "python", "js", "java", "python", "java", "C++", "c"}
+classes.union
+print(len(classes))
+print(f"required classes are {len(classes)}.")
+
+
+
+
+
+
+
+
+
+
